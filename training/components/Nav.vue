880adce255291a1717caa3c29cbd1453c3e57c1d
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-04 19:16:24 +0100

    nav

--- training/components/Nav.vue
+++ training/components/Nav.vue
@@ -1,6 +1,37 @@
 <template>
   <div>
-      
+      <div className="navi">
+            <div className="header__icons">
+                    <div className="header__icon header__icon--active">
+                        <HomeIcon/>
+                        <p>Home</p>
+                    </div>
+            </div>
+            <div className="header__icons">
+                <div className="header__icon header__icon--active">
+                    <SupervisorAccountIcon/>
+                    <p>Contestant<span><ArrowDropDownIcon/></span></p>
+                </div>
+            </div>
+            <div className="header__icons">
+                <div className="header__icon header__icon--active">
+                    <ThumbsUpIcon/>
+                    <p>Vote</p>
+                </div>
+            </div>
+            <div className="header__icons">
+                <div className="header__icon header__icon--active">
+                    <ShoppingBasketIcon/>
+                    <p>Products</p>
+                </div>
+            </div>
+            <div className="header__icons">
+                <div className="header__icon header__icon--active">
+                    <CardGiftcardIcon/>
+                    <p>Support</p>
+                </div>
+            </div>
+        </div>
   </div>
 </template>
 
