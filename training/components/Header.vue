6fcd4eaca7ac721b07d3edcda86eda6f612e1465
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-04 19:08:49 +0100

    h

--- training/components/Header.vue
+++ training/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
    <div className="header">
             
-            <div className="btn-sign">
+            <div class="btn-sign">
                 <Button color= "primary" variant="outline-light"><span>Login</span></Button>
             </div>
             
@@ -17,5 +17,90 @@ export default {
 </script>
 
 <style>
+    .header {
+    color: white !important;
+    display: flex!important;
+    padding: 20px!important;
+    align-items: center!important;
+    justify-content: space-between!important;
+    background-image: linear-gradient(to right, #051421, #042727);
+   
+}
 
+/*Removing scroll*/
+.header::-webkit-scrollbar {
+    display: none!important;
+}
+
+.header {
+    -ms-overflow-style: none !important;
+    scrollbar-width: none !important;
+}
+/*Removing scroll*/
+
+.header__icons {
+    display: flex;
+    align-items: center;
+    text-align: center;
+}
+
+.header > img {
+    object-fit: contain;
+    height: 80px;
+}
+
+.header__icon {
+    display: flex;
+    flex-direction: column;
+    cursor: pointer;
+    width: 100px;
+}
+
+.header__icon > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: auto;
+    font-size: 30px;
+}
+
+.header__icon > p {
+    display: none;
+    text-transform: uppercase;
+    letter-spacing: 3px;
+    margin-left: auto;
+    margin-right: auto;
+    font-size: small;
+}
+
+.header__icon:hover > p {
+    display: inline-flex;
+}
+
+.header__icon--active > p {
+    display: inline-flex;
+}
+
+h1 {
+    letter-spacing: 2px!important;
+    margin-right: auto!important;
+    font-weight: 400;
+}
+
+.btn-sign {
+    padding: 5px !important;
+    display: flex !important;
+    flex-direction: column !important;
+}
+
+.btn-sign:hover {
+    box-shadow: white !important;
+}
+
+div > Button {
+    display: none;
+    text-transform: uppercase;
+    letter-spacing: 3px;
+    margin-left: auto;
+    margin-right: auto;
+    font-size: small;
+}
 </style>
