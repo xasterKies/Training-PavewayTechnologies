27fa88a6c5d579b8c689e7379d937bde91581388
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-07 08:55:05 +0100

    h

--- training/components/Header.vue
+++ training/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
    <div class="header">
-        <nav class="navbar navbar-light navbar-fixed-top navbar-expand-lg" style="background-color: #e3f2fd;" >
+        <nav class="navbar navbar-light navbar-fixed-top " >
         <div class="container">
             <div class="navbar-header">
                 <!------------------Responsive button --------------------->
