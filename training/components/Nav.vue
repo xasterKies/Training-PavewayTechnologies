3f33c69d8a28439fd89653051d60be7f6e63c0f8
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-07 12:55:40 +0100

    n

--- training/components/Nav.vue
+++ training/components/Nav.vue
@@ -1,20 +1,20 @@
 <template>
       <center>
-        <div className="navi">
-            <div className="header__icons">
-                    <div className="header__icon header__icon--active">
+        <div class="navi">
+            <div class="header__icons">
+                    <div class="header__icon header__icon--active">
                         <HomeIcon/>
                         <p>Home</p>
                     </div>
             </div>
-            <div className="header__icons">
-                <div className="header__icon header__icon--active">
+            <div class="header__icons">
+                <div class="header__icon header__icon--active">
                     <SupervisorAccountIcon/>
                     <p>Contestant<span><ArrowDropDownIcon/></span></p>
                 </div>
             </div>
-            <div className="header__icons">
-                <div className="header__icon header__icon--active">
+            <div class="header__icons">
+                <div class="header__icon header__icon--active">
                     <ThumbsUpIcon/>
                     <p>Vote</p>
                 </div>
@@ -34,68 +34,9 @@ export default {
 </script>
 
 <style>
-    .navi {
-    display: flex!important;
-    color: white!important;
-    overflow-x: scroll!important;
-    padding: 10px!important;
-    padding-left: 40px!important;
-}
-
-/*Hiding scrollbar*/
-.navi::-webkit-scrollbar {
-    display: none!important;
-}
-
-.navi {
-    -ms-overflow-style: none!important;
-    scrollbar-width: none !important;
-}
-/*Hiding the scrollbar*/
-
-.navi > div {
-    cursor: pointer;
-    margin-left: 30px;
-    margin-right: 30px;
-    font-weight: 400;
-    opacity: 0.7;
-    transition: transform 70ms ease-out;
-    font-size: 20px;
-}
-
-.navi > div:hover {
-    opacity: 1;
-    transform: scale(1.2)
-}
-
-.header__icons {
-    display: flex;
-    align-items: center;
-    text-align: center;
-}
-
-
-.header__icon {
-    display: flex;
-    flex-direction: column;
-    cursor: pointer;
-    width: 100px;
-}
-
-.header__icon > .MuiSvgIcon-root {
-    margin-left: auto;
-    margin-right: auto;
-    font-size: 30px;
-}
-
-.header__icon > p {
-    display: none;
-    text-transform: uppercase;
-    letter-spacing: 3px;
-    margin-left: auto;
-    margin-right: auto;
-    font-size: small;
-}
+   .navi {
+       margin-top: 50px
+   }
 
 
 </style>
