4e7549753a5cd9fd93d3f1c243eaa1f157eeec93
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-07 16:22:08 +0100

    n

--- training/components/Nav.vue
+++ training/components/Nav.vue
@@ -1,23 +1,35 @@
 <template>
       <center>
         <div class="navi">
-            <div class="header__icons">
-                    <div class="header__icon header__icon--active">
-                        <HomeIcon/>
-                        <p>Home</p>
-                    </div>
-            </div>
-            <div class="header__icons">
-                <div class="header__icon header__icon--active">
-                    <SupervisorAccountIcon/>
-                    <p>Contestant<span><ArrowDropDownIcon/></span></p>
+            <div className="header__icons">
+                <div className="header__icon header__icon--active">
+                    <HomeIcon/>
+                    <p>Home</p>
                 </div>
-            </div>
-            <div class="header__icons">
-                <div class="header__icon header__icon--active">
-                    <ThumbsUpIcon/>
-                    <p>Vote</p>
+                <div className="header__icon">
+                    <FlashOnIcon/>
+                    <p>Trending</p>
                 </div>
+                <div className="header__icon">
+                    <LiveTvIcon/>
+                    <p>Live Tv</p>
+                </div>
+                <div className="header__icon">
+                    <VideoLibraryIcon/>
+                    <p>Collections</p>
+                </div>
+                <div className="header__icon">
+                    <SearchIcon/>
+                    <p>Search</p>
+                </div>
+                <div className="header__icon">
+                    <PersonOutlineIcon/>
+                    <p>Account</p>
+                </div>
+                
+                
+                
+
             </div>
             
         </div>
@@ -35,8 +47,70 @@ export default {
 
 <style>
    .navi {
-       margin-top: 50px;
+       margin: 50px;
+       overflow: hidden;
    }
+
+   .navi {
+    color: white;
+    display: flex;
+    padding: 20px;
+    align-items: center;
+    justify-content: space-between;
+    overflow-x: scroll;
+}
+
+/*Removing scroll*/
+.header::-webkit-scrollbar {
+    display: none;
+}
+
+.header {
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+}
+/*Removing scroll*/
+
+.header__icons {
+    display: flex;
+    align-items: center;
+    text-align: center;
+}
+
+.header > img {
+    object-fit: contain;
+    height: 80px;
+}
+
+.header__icon {
+    display: flex;
+    flex-direction: column;
+    cursor: pointer;
+    width: 100px;
+}
+
+.header__icon > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: auto;
+    font-size: 30px;
+}
+
+.header__icon > p {
+    display: none;
+    text-transform: uppercase;
+    letter-spacing: 3px;
+    margin-left: auto;
+    margin-right: auto;
+    font-size: small;
+}
+
+.header__icon:hover > p {
+    display: inline-flex;
+}
+
+.header__icon--active > p {
+    display: inline-flex;
+}
 
 
 </style>
