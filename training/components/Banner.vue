2f78a3ac83e449c06bc2d3f8b11aede2fcc6188a
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-08 20:14:20 +0100

    banner

---
+++ training/components/Banner.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+      <section class="slider text-center" id="slider">
+            <div class="slider-overlay">
+                <div class="slider-content">
+                    
+                    <br>
+                    <div class="text"></div>
+                    <br><br>
+                    <div class="cta-div">
+                        <a href="" class="btn1"><span class="con-text">Contact Us</span></a>
+                    </div>
+                    <br>
+                    <br>
+                    
+
+                </div>
+
+            </div>
+
+
+        </section>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
